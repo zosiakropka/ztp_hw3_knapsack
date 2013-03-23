0d6bda5cc471ba8a0d9426c1c6931bdbbfbc9ef0
Zosia <sobocinska.zosia@gmail.com>
2013-03-23 15:58:50 +0100

    Project starter

---
+++ Knapsack.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author Zofia Sobocinska
+ */
+public class Knapsack {
+
+	public int pack() {
+		return 0;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author Zofia Sobocinska
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("usage: java Main <input_file> <square_size>");
+		}
+	}
+}
